3cb30df756804dc8318c69b76d65df7c7e2a2ced
Tom Oldani <tom@ohcintl.com>
2013-06-21 08:18:35 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ediamonds::Application.config.secret_token = 'd5120d6ad86839af3b97ba687990eec19c56ebd7859de1dae3ddfac3d1dbe461266960b769a929f1bb6e1cebf4518e53e47b9af31e348e68f326e5a45e370b61'
